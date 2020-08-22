cc4bb215ba1563a8c25924e1ecff7911fe4cd307
Gustav <gustavfors@outlook.com>
2020-08-22 10:01:01 +0100

    Created a simple router

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+$routes = [
+    '' => 'home.php'
+];
+
+$uri = trim($_SERVER['REQUEST_URI'], '/');
+
+if (array_key_exists($uri, $routes)) {
+    require "{$routes[$uri]}";
+} else {
+    die('404 page not found.');
+}
