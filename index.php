9c809ced364429b1fc56c2042561e38edf013a31
Gustav <gustavfors@outlook.com>
2020-08-22 10:17:06 +0100

    Refactored router logic into a Router class

--- index.php
+++ index.php
@@ -1,16 +1,33 @@
 <?php
 
-$routes = [
-    '' => 'home.php',
-    'about' => 'about.php',
-    'contact' => 'contact.php'
-];
+class Router
+{
+    protected $routes = [];
+
+    public function register($uri, $view)
+    {
+        $this->routes[$uri] = $view;
+    }
+
+    public function direct($uri)
+    {
+        if (array_key_exists($uri, $this->routes)) {
+            return $this->routes[$uri];
+        }
+
+        die("404 page not found.");
+    }
+}
+
+
+$router = new Router();
+
+$router->register('', 'home.php');
+$router->register('about', 'about.php');
+$router->register('contact', 'contact.php');
 
 
 $uri = trim($_SERVER['REQUEST_URI'], '/');
 
-if (array_key_exists($uri, $routes)) {
-    require "{$routes[$uri]}";
-} else {
-    die('404 page not found.');
-}
+require $router->direct($uri);
+
