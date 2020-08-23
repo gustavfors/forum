2c2b9cc4bc8cfff9f9191108b25e1a12332c00f2
Gustav <gustavfors@outlook.com>
2020-08-23 08:57:03 +0100

    Added test route for experimenting with POST

--- routes/routes.php
+++ routes/routes.php
@@ -3,3 +3,4 @@
 $router->register('', 'controllers/index.php');
 $router->register('about', 'controllers/about.php');
 $router->register('contact', 'controllers/contact.php');
+$router->register('test', 'controllers/test.php');
--- views/index.view.php
+++ views/index.view.php
@@ -1,1 +1,6 @@
 <p>This is the <?= $name; ?> page</p>
+
+<form action="/test" method="POST" autocomplete="off">
+    <input type="text" name="name">
+    <button type="submit">submit</button>
+</form>
