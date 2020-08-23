1b50cfa2e2d090f7508191ad80dba8a6ac296017
Gustav <gustavfors@outlook.com>
2020-08-23 09:30:30 +0100

    refactor, moved request logic into dedicated class

--- bootstrap/bootstrap.php
+++ bootstrap/bootstrap.php
@@ -1,7 +1,11 @@
 <?php
 
+//load models
 require 'models/Router.php';
+require 'models/Request.php';
 
+//create router
 $router = new Router();
 
+//load routes
 require 'routes/routes.php';
--- index.php
+++ index.php
@@ -2,9 +2,4 @@
 
 require 'bootstrap/bootstrap.php';
 
-$uri = trim($_SERVER['REQUEST_URI'], '/');
-$requestMethod = $_SERVER['REQUEST_METHOD'];
-
-require $router->direct($uri, $requestMethod);
-
-
+require $router->direct(Request::uri(), Request::method());
